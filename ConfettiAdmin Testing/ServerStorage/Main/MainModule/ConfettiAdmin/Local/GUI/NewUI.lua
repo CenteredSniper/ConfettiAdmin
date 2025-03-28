be53b7355ca2d5a0bbea39faa669da0d85e18369
timedmarch <59752762+CenteredSniper@users.noreply.github.com>
2025-03-28 07:18:09 -0700

    Updated ConfettiAdmin Testing/ServerStorage/Main/MainModule/ConfettiAdmin/Local/GUI/NewUI.lua

---
+++ ConfettiAdmin Testing/ServerStorage/Main/MainModule/ConfettiAdmin/Local/GUI/NewUI.lua
@@ -0,0 +1,20 @@
+-- @ScriptType: ModuleScript
+local Module = {}
+
+function Module:Initilize(shared, Name, Data)
+	local UiElements = shared.SharedAssets:WaitForChild("UiElements", 1/3)
+	
+	if UiElements then
+		local Clone = UiElements:WaitForChild(Name, 1/3)
+		local UiModule = script:WaitForChild(Name,5)
+		
+		if Clone and UiModule then
+			Clone = Clone:Clone()
+			
+			Clone.Parent = shared.Gui
+			require(UiModule):Initilize(shared, Clone, Data)
+		end
+	end
+end
+
+return Module
