6adfcb42f78783c668b59c242edeb9eba7cba104
timedmarch <59752762+CenteredSniper@users.noreply.github.com>
2025-03-28 07:18:49 -0700

    Updated ConfettiAdmin Testing/ServerStorage/Main/MainModule/ConfettiAdmin/Shared/VoidScripts/RavengerClaws.lua

---
+++ ConfettiAdmin Testing/ServerStorage/Main/MainModule/ConfettiAdmin/Shared/VoidScripts/RavengerClaws.lua
@@ -0,0 +1,14 @@
+-- @ScriptType: ModuleScript
+local Module = {}
+
+Module.Metadata = {
+	Name = "Ravenger Claws V2",
+	Description = "",
+	Creator = "Supr14?",
+}
+
+function Module:Initilize(shared, Player)
+	
+end
+
+return Module
