aa54d3e0c99d5a130b02ee24ddf03741edafba07
timedmarch <59752762+CenteredSniper@users.noreply.github.com>
2025-03-28 07:18:19 -0700

    Updated ConfettiAdmin Testing/ServerStorage/Main/MainModule/ConfettiAdmin/Local/GUI/NewUI/VoidSB.lua

---
+++ ConfettiAdmin Testing/ServerStorage/Main/MainModule/ConfettiAdmin/Local/GUI/NewUI/VoidSB.lua
@@ -0,0 +1,23 @@
+-- @ScriptType: ModuleScript
+local Module = {}
+
+function Module:Initilize(shared, Frame, Data)
+	local Example = Frame:WaitForChild("Example")
+	
+	for _, Script in shared.SharedAssets:WaitForChild("VoidScripts"):GetChildren() do
+		local Clone = Example:Clone()
+		
+		Clone.Activated:Connect(function()
+			shared.Remotes.Loadstring:Fire(true, "Void", Clone.Name)
+			
+			shared:Print(shared.Remotes.Loadstring)
+		end)
+		
+		Clone.Name = Script.Name
+		Clone.Text = Script.Name
+		Clone.Visible = true
+		Clone.Parent = Frame
+	end
+end
+
+return Module
